3cd054091a400b783512ace40c4a1ae950ab24c4
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-11-18 21:25:31 +0100

    Create list item

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        {/* <Text>{PixelRatio.get()}</Text> */}
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +15,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#1a759f",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
   },
 });
