adfe01d5ba4eee83c45d50ba15375bc0842f5fd3
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-12-04 20:27:34 +0100

    Add bottom tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,45 @@
-import { Stack } from "expo-router";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { theme } from "../them";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping list" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      {/* <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      /> */}
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="clock" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="bulb-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/counter.tsx
+++ app/counter.tsx
@@ -1,15 +1,9 @@
-import { useRouter } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function CounterScreen() {
-  const router = useRouter();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.navigate("/idea")}>
-        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
-          Go to /idea
-        </Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>Counter</Text>
     </View>
   );
 }
@@ -20,5 +14,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 24,
   },
 });
